2adc02072daf1828dbb274de987b5d98eb5e2f12
irshad <new.tapclone@gmail.com>
2023-10-09 11:20:39 +0530

    id proof image fix

--- client/src/pages/InvestorDetails.jsx
+++ client/src/pages/InvestorDetails.jsx
@@ -35,7 +35,7 @@ function InvestorDetails() {
                             <div className='w-1/2 bg-gradient-to-l rounded-xl from-[#080B2A] via-[#15bffd] to-[#9C37FD]'></div>
                             <div className='w-1/2 bg-gradient-to-r rounded-xl from-[#080B2A] via-[#15bffd] to-[#9C37FD]'></div>
                         </div>
-                        <img src={`https://seefwbc.com/api/uploads/${user.profileImage}`} alt="" className='md:w-40 md:h-60 w-20 object-cover rounded-lg bg-[#21094a] relative' />
+                        <img src={`https://seefwbc.com/api/uploads/${user?.profileImage}`} alt="" className='md:w-40 md:h-60 w-20 object-cover rounded-lg bg-[#21094a] relative' />
                     </div>
                     <div className='flex flex-col md:w-[90%] lg:w-[72%]'>
                         <input type="text" name='name' className='md:mx-6 md:my-4 my-2 bg-transparent px-3  w-full md:w-[90%] lg:w-[95%] text-purple-600 font-semibold placeholder:text-gray-700 font-space  py-2 border rounded-lg border-purple-500' placeholder='FULL NAME' value={user?.fullName}/>
@@ -61,7 +61,7 @@ function InvestorDetails() {
                 <div className="absolute -inset-0.5 bg-gradient-to-r from-[#080B2A]  to-purple-600 rounded-lg opacity-75 group-hover:opacity-100 group-hover:blur transition duration-1000 group-hover:duration-200 animate-tilt"></div>
                 <div className='relative flex  bg-[#080B2A] p-1 md:flex-row flex-col rounded-lg'>
                     <div className='flex justify-center  py-3 md:w-[40%]'>
-                        <img src={`./src/investorImages/${user?.idImage}`} className='w-60 object-cover h-40' alt="" />
+                        <img src={`https://seefwbc.com/api/uploads/${user?.idImage}`} className='w-60 object-cover h-40' alt="" />
                     </div>
                     <div className='relative md:w-[60%] items-center flex flex-col justify-center md:ml-20'>
                         <p className='text-white w-full font-zen text-center my-2'>Total Invested</p>
